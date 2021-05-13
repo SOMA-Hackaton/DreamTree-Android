e27fc36282afdfa5cfdd43636e93a35f4aa932a1
H43RO <h2is1234@naver.com>
2021-05-13 16:35:49 +0900

    [feat] DataBinding RecyclerView Adapter 구현
    DataBinding 이 적용된 RecyclerViewAdapter, ViewHolder 등을
    구현했습니다. 이는 꿈나무 카드 가맹점 목록을 보여주기 위한 어댑터로 사용됩니다.

---
+++ app/src/main/java/com/soma_quokka/dreamtree/adapter/StoreListAdapter.kt
@@ -0,0 +1,40 @@
+package com.soma_quokka.dreamtree.adapter
+
+import android.view.LayoutInflater
+import android.view.ViewGroup
+import androidx.recyclerview.widget.RecyclerView
+import com.soma_quokka.dreamtree.BR
+import com.soma_quokka.dreamtree.databinding.StoreListItemBinding
+
+class StoreListAdapter(
+) : RecyclerView.Adapter<StoreListAdapter.ViewHolder>() {
+    private var items: List<Store> = listOf()
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ViewHolder {
+        val biding = StoreListItemBinding.inflate(LayoutInflater.from(parent.context), parent, false)
+
+        return ViewHolder(binding)
+    }
+
+    override fun onBindViewHolder(holder: ViewHolder, position: Int) {
+        holder.bind(items[position])
+    }
+
+    override fun getItemCount(): Int {
+        return items.size
+    }
+
+    inner class ViewHolder(
+        private val binding: StoreListItemBinding
+    ) : RecyclerView.ViewHolder (){
+
+        fun bind(item: Store) {
+            binding.setVariable(BR.store, store)
+        }
+    }
+
+    fun setItems(items: List<Store>) {
+        this.items = items
+        notifyDataSetChanged()
+    }
+}
