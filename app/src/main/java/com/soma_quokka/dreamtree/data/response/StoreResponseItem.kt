381bfcdefbd87b8b20a1c9299279667656b09d36
Haemin-Park <phm9368@sookmyung.ac.kr>
2021-05-13 21:59:07 +0900

    [feat] 임시 리스폰스 모델 생성

---
+++ app/src/main/java/com/soma_quokka/dreamtree/data/response/StoreResponseItem.kt
@@ -0,0 +1,20 @@
+package com.soma_quokka.dreamtree.data.response
+
+import android.annotation.SuppressLint
+import kotlinx.android.parcel.Parcelize
+import android.os.Parcelable
+
+@SuppressLint("ParcelCreator")
+@Parcelize
+data class StoreResponseItem(
+    val address: String,
+    val dong: String,
+    val imgurl: String,
+    val latitude: Double,
+    val longitude: Double,
+    val menus: List<Menu>,
+    val name: String,
+    val phoneNumber: String,
+    val rating: Double,
+    val type: String
+) : Parcelable
